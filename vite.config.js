ea9ceabf9e09052543bcc1fa3f503df0b0d47780
Ka1 <rosezaka46@gmail.com>
2026-03-01 10:10:08 +0700

    Configure base url for github pages without custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // reverted to root path because custom domain (r1zky.com) is used
-  base: '/',
+  // use repo name as base path if not using custom domain on a non-root repo
+  base: '/r1zky.github.io/',
 })
