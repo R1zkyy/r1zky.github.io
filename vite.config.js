ba879eade116354c22d1afea28f215ea5554c283
Ka1 <rosezaka46@gmail.com>
2026-03-01 10:13:38 +0700

    Remove base routing for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // use repo name as base path if not using custom domain on a non-root repo
-  base: '/r1zky.github.io/',
+  // removed base completely as Vercel handles routing from root automatically
 })
