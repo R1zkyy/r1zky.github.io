3586ac8ecba90d1f907631c57062d12504b21310
Ka1 <rosezaka46@gmail.com>
2026-03-01 09:53:53 +0700

    Fix base url for r1zky.github.io repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base path is '/' for User/Organization pages (e.g., https://<username>.github.io/)
-  base: '/',
+  base: '/r1zky.github.io/',
 })
